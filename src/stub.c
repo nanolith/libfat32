ebad91342e10a9fe4a01614a75e53928d718661c
Justin Handville <nanolith@gmail.com>
2025-10-20 02:27:44 -0400

    stub source file.

---
+++ src/stub.c
@@ -0,0 +1,11 @@
+/**
+ * \file stub.c
+ *
+ * \brief Stub for verifying headers.
+ *
+ * \copyright 2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libfat32/gpt.h>
+#include <stdio.h>
