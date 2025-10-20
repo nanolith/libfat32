781575e6b7e9a93ce9434cba520c84591c37a260
Justin Handville <nanolith@gmail.com>
2025-10-20 02:27:53 -0400

    stub unit test file.

---
+++ test/stub.cpp
@@ -0,0 +1,10 @@
+/**
+ * \file test/stub.cpp
+ *
+ * \brief Stub test for libfat32.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <minunit/minunit.h>
