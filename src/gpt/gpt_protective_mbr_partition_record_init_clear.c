1de3ec513f7e8885571c61379e2ebcae73b800ec
Justin Handville <nanolith@gmail.com>
2025-10-21 19:15:39 -0400

    gpt_protective_mbr_partition_record_init_clear impl.

---
+++ src/gpt/gpt_protective_mbr_partition_record_init_clear.c
@@ -0,0 +1,30 @@
+/**
+ * \file gpt/gpt_protective_mbr_partition_record_init_clear.c
+ *
+ * \brief Initialize an empty protective mbr partition record.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libfat32/gpt.h>
+#include <libfat32/status.h>
+#include <string.h>
+
+/**
+ * \brief Initialize an empty protective mbr partition record.
+ *
+ * \param rec               The record to initialize.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int FN_DECL_MUST_CHECK
+FAT32_SYM(gpt_protective_mbr_partition_record_init_clear)(
+    FAT32_SYM(gpt_protective_mbr_partition_record)* rec)
+{
+    memset(rec, 0, sizeof(*rec));
+
+    return STATUS_SUCCESS;
+}
