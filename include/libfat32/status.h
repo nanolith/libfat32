b2ebbd915c725dc66155357b465fd29253e4d0e8
Justin Handville <nanolith@gmail.com>
2025-10-20 23:52:42 -0400

    FAT32_ERROR_GUID_DATA_TOO_SMALL decl.

--- include/libfat32/status.h
+++ include/libfat32/status.h
@@ -17,6 +17,7 @@ extern "C" {
 enum fat32_status_codes
 {
     STATUS_SUCCESS =                                                        0,
+    FAT32_ERROR_GUID_DATA_TOO_SMALL =                                       1,
 };
 
 /* C++ compatibility. */
