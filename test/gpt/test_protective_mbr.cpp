29154b32efaf1279b51ceaea02d83da81d38136a
Justin Handville <nanolith@gmail.com>
2025-10-21 19:15:59 -0400

    gpt_protective_mbr_partition_record_init_clear unit test.

---
+++ test/gpt/test_protective_mbr.cpp
@@ -0,0 +1,41 @@
+/**
+ * \file test/gpt/test_protective_mbr.cpp
+ *
+ * \brief Unit tests for protective MBR interface.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libfat32/gpt.h>
+#include <libfat32/status.h>
+#include <minunit/minunit.h>
+#include <string.h>
+
+FAT32_IMPORT_gpt;
+
+TEST_SUITE(protective_mbr);
+
+/**
+ * We can initialize an empty MBR partition record.
+ */
+TEST(gpt_protective_mbr_partition_record_init_clear)
+{
+    gpt_protective_mbr_partition_record rec;
+
+    /* precondition: fill with junk. */
+    memset(&rec, 0xa5, sizeof(rec));
+
+    /* initialize this data structure. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == gpt_protective_mbr_partition_record_init_clear(&rec));
+
+    /* verify the partition record data. */
+    TEST_EXPECT(0 == rec.boot_indicator);
+    TEST_EXPECT(0 == rec.starting_chs);
+    TEST_EXPECT(0 == rec.os_type);
+    TEST_EXPECT(0 == rec.ending_chs);
+    TEST_EXPECT(0 == rec.starting_lba);
+    TEST_EXPECT(0 == rec.size_in_lba);
+}
