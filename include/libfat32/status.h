7e5cb8b63b453d10f5dbfb5b5839a66ded88d93a
Justin Handville <nanolith@gmail.com>
2025-10-20 23:52:00 -0400

    STATUS_SUCCESS decl.

---
+++ include/libfat32/status.h
@@ -0,0 +1,25 @@
+/**
+ * \file libfat32/status.h
+ *
+ * \brief Status codes for libfat32.
+ *
+ * \copyright 2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+enum fat32_status_codes
+{
+    STATUS_SUCCESS =                                                        0,
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
