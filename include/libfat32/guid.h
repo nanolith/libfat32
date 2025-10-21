67ed27c127f2cc622f695bba01ccbaf42a85665d
Justin Handville <nanolith@gmail.com>
2025-10-20 23:38:46 -0400

    guid_write_to_binary decl.

--- include/libfat32/guid.h
+++ include/libfat32/guid.h
@@ -85,6 +85,21 @@ int FN_DECL_MUST_CHECK
 FAT32_SYM(guid_write_to_string)(
     char* str, size_t size, const FAT32_SYM(guid)* id);
 
+/**
+ * \brief Write the guid representation to the given binary buffer.
+ *
+ * \param buffer            The binary buffer.
+ * \param size              The size of the binary buffer.
+ * \param id                The guid to write.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error on failure.
+ */
+int FN_DECL_MUST_CHECK
+FAT32_SYM(guid_write_to_binary)(
+    void* buffer, size_t size, const FAT32_SYM(guid)* id);
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
