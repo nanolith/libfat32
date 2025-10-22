5842aec635cff1f6ff85c1c3c1a1c9138de6a7e6
Justin Handville <nanolith@gmail.com>
2025-10-21 22:00:21 -0400

    gpt_protective_mbr_partition_record_read impl.

--- src/gpt/gpt_protective_mbr_partition_record_read.c
+++ src/gpt/gpt_protective_mbr_partition_record_read.c
@@ -9,6 +9,7 @@
 
 #include <libfat32/gpt.h>
 #include <libfat32/status.h>
+#include <string.h>
 
 /**
  * \brief Read a protective mbr partition record from a given location in RAM.
@@ -32,7 +33,43 @@ FAT32_SYM(gpt_protective_mbr_partition_record_read)(
         return FAT32_ERROR_GPT_BAD_SIZE;
     }
 
-    (void)rec;
-    (void)ptr;
-    return -1;
+    /* clear record. */
+    memset(rec, 0, sizeof(*rec));
+
+    /* make working with this memory more convenient. */
+    const uint8_t* bptr = (const uint8_t*)ptr;
+
+    /* read the boot indicator. */
+    rec->boot_indicator = bptr[0];
+
+    /* read the starting chs as little endian. */
+    rec->starting_chs = 0;
+    rec->starting_chs |= ((uint32_t)bptr[1]);
+    rec->starting_chs |= ((uint32_t)bptr[2]) <<  8;
+    rec->starting_chs |= ((uint32_t)bptr[3]) << 16;
+
+    /* read the OS type record. */
+    rec->os_type = bptr[4];
+
+    /* read the ending chs as little endian. */
+    rec->ending_chs = 0;
+    rec->ending_chs |= ((uint32_t)bptr[5]);
+    rec->ending_chs |= ((uint32_t)bptr[6]) <<  8;
+    rec->ending_chs |= ((uint32_t)bptr[7]) << 16;
+
+    /* read the starting lba as little endian. */
+    rec->starting_lba = 0;
+    rec->starting_lba |= ((uint32_t)bptr[ 8]);
+    rec->starting_lba |= ((uint32_t)bptr[ 9]) <<  8;
+    rec->starting_lba |= ((uint32_t)bptr[10]) << 16;
+    rec->starting_lba |= ((uint32_t)bptr[11]) << 24;
+
+    /* read the size in lba as little endian. */
+    rec->size_in_lba = 0;
+    rec->size_in_lba |= ((uint32_t)bptr[12]);
+    rec->size_in_lba |= ((uint32_t)bptr[13]) <<  8;
+    rec->size_in_lba |= ((uint32_t)bptr[14]) << 16;
+    rec->size_in_lba |= ((uint32_t)bptr[15]) << 24;
+
+    return STATUS_SUCCESS;
 }
