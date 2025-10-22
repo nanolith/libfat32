adda759fbc8081f89adfb040d3bdccab43f5c7a1
Justin Handville <nanolith@gmail.com>
2025-10-21 21:14:46 -0400

    gpt_protective_mbr_partition_record_write impl.

--- src/gpt/gpt_protective_mbr_partition_record_write.c
+++ src/gpt/gpt_protective_mbr_partition_record_write.c
@@ -9,6 +9,7 @@
 
 #include <libfat32/gpt.h>
 #include <libfat32/status.h>
+#include <string.h>
 
 /**
  * \brief Write a protective mbr partition record to a given location in RAM.
@@ -32,7 +33,38 @@ FAT32_SYM(gpt_protective_mbr_partition_record_write)(
         return FAT32_ERROR_GPT_BAD_SIZE;
     }
 
-    (void)ptr;
-    (void)rec;
-    return -1;
+    uint8_t* bptr = (uint8_t*)ptr;
+
+    /* clear memory. */
+    memset(ptr, 0, size);
+
+    /* write the boot indicator. */
+    bptr[0] = rec->boot_indicator;
+
+    /* write the starting chs as little endian. */
+    bptr[1] = ((rec->starting_chs      ) & 0xFF);
+    bptr[2] = ((rec->starting_chs >>  8) & 0xFF);
+    bptr[3] = ((rec->starting_chs >> 16) & 0xFF);
+
+    /* write the OS type record. */
+    bptr[4] = rec->os_type;
+
+    /* write the ending chs as little endian. */
+    bptr[5] = ((rec->ending_chs      ) & 0xFF);
+    bptr[6] = ((rec->ending_chs >>  8) & 0xFF);
+    bptr[7] = ((rec->ending_chs >> 16) & 0xFF);
+
+    /* write the starting lba as little endian. */
+    bptr[ 8] = ((rec->starting_lba      ) & 0xFF);
+    bptr[ 9] = ((rec->starting_lba >>  8) & 0xFF);
+    bptr[10] = ((rec->starting_lba >> 16) & 0xFF);
+    bptr[11] = ((rec->starting_lba >> 24) & 0xFF);
+
+    /* write the size in lba as little endian. */
+    bptr[12] = ((rec->size_in_lba      ) & 0xFF);
+    bptr[13] = ((rec->size_in_lba >>  8) & 0xFF);
+    bptr[14] = ((rec->size_in_lba >> 16) & 0xFF);
+    bptr[15] = ((rec->size_in_lba >> 24) & 0xFF);
+
+    return STATUS_SUCCESS;
 }
