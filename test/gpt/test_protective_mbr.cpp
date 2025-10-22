0526b0885c446728d19d5271c3f7335a1ba78a98
Justin Handville <nanolith@gmail.com>
2025-10-21 20:28:37 -0400

    gpt_protective_mbr_partition_record_init_span_oversize_4TB unit test.

--- test/gpt/test_protective_mbr.cpp
+++ test/gpt/test_protective_mbr.cpp
@@ -66,3 +66,28 @@ TEST(gpt_protective_mbr_partition_record_init_span_128GB)
     TEST_EXPECT(0x00000001 == rec.starting_lba);
     TEST_EXPECT(lba_size == rec.size_in_lba);
 }
+
+/**
+ * We can initialize an "oversize" "span" mbr partition record covering 4TB.
+ */
+TEST(gpt_protective_mbr_partition_record_init_span_oversize_4TB)
+{
+    gpt_protective_mbr_partition_record rec;
+    const size_t disk_size = 4UL * 1024UL * 1024UL * 1024UL * 1024UL;
+
+    /* precondition: fill with junk. */
+    memset(&rec, 0xa5, sizeof(rec));
+
+    /* initialize this data structure. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == gpt_protective_mbr_partition_record_init_span(&rec, disk_size));
+
+    /* verify the partition record data. */
+    TEST_EXPECT(0 == rec.boot_indicator);
+    TEST_EXPECT(0x00000200 == rec.starting_chs);
+    TEST_EXPECT(0xEE == rec.os_type);
+    TEST_EXPECT(0x00FFFFFF == rec.ending_chs);
+    TEST_EXPECT(0x00000001 == rec.starting_lba);
+    TEST_EXPECT(0xFFFFFFFF == rec.size_in_lba);
+}
