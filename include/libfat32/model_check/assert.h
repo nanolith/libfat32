b4d74709e55bdbb4dd7910404f08be950e453cfe
Justin Handville <nanolith@gmail.com>
2025-10-24 01:37:26 -0400

    MODEL_ASSERT decl.

---
+++ include/libfat32/model_check/assert.h
@@ -0,0 +1,20 @@
+/**
+ * \file libfat32/model_check/assert.h
+ *
+ * \brief Macros for handling model check assertions, assumptions, and
+ * exceptions in source code.
+ *
+ * \copyright 2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <assert.h>
+#include <libfat32/macro_tricks.h>
+
+#ifdef CBMC
+#define MODEL_ASSERT(x) __CPROVER_assert((x), #x); REQUIRE_SEMICOLON_HERE
+#else
+#define MODEL_ASSERT(x) REQUIRE_SEMICOLON_HERE
+#endif
