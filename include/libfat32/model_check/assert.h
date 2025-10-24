5dd623c2d6e6cbccf60da55f42b0ee70f9103df8
Justin Handville <nanolith@gmail.com>
2025-10-24 01:38:33 -0400

    MODEL_EXEMPT decl.

--- include/libfat32/model_check/assert.h
+++ include/libfat32/model_check/assert.h
@@ -16,7 +16,9 @@
 #ifdef CBMC
 #define MODEL_ASSERT(x) __CPROVER_assert((x), #x); REQUIRE_SEMICOLON_HERE
 #define MODEL_ASSUME(x) __CPROVER_assume((x)); REQUIRE_SEMICOLON_HERE
+#define MODEL_EXEMPT(x)
 #else
 #define MODEL_ASSERT(x) REQUIRE_SEMICOLON_HERE
 #define MODEL_ASSUME(x) REQUIRE_SEMICOLON_HERE
+#define MODEL_EXEMPT(x) x
 #endif
