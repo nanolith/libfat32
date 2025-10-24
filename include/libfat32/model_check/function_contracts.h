b58958b0ff580b9967ad9b321ad3c4f2c2b2c5bd
Justin Handville <nanolith@gmail.com>
2025-10-24 02:05:57 -0400

    MODEL_CONTRACT_PRECONDITIONS_BEGIN1 decl.

---
+++ include/libfat32/model_check/function_contracts.h
@@ -0,0 +1,18 @@
+/**
+ * \file libfat32/model_check/function_contracts.h
+ *
+ * \brief Macros for handling function contract declarations and assertions.
+ *
+ * \copyright 2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <assert.h>
+#include <libfat32/function_decl.h>
+#include <libfat32/macro_tricks.h>
+
+#define MODEL_CONTRACT_PRECONDITIONS_BEGIN1(function, ...) \
+    FAT32_BEGIN_CONTRACT_HELPER \
+    inline void function ## _preconditions(__VA_ARGS__) {
