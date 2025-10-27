c2606b556e89c0ef0a7bf8b43b8b176244e0766e
Justin Handville <nanolith@gmail.com>
2025-10-26 20:59:24 -0400

    gpt_protective_mbr_partition_record_init_span model checks.

---
+++ models/gpt/gpt_protective_mbr_partition_record_init_span/main.c
@@ -0,0 +1,35 @@
+/**
+ * \file models/gpt/gpt_protective_mbr_partition_record_init_span/main.c
+ *
+ * \brief Model checks for \ref gpt_protective_mbr_partition_record_init_span.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <libfat32/model_check/assert.h>
+#include <libfat32/status.h>
+#include <libfat32/gpt.h>
+
+FAT32_IMPORT_gpt;
+
+size_t nondet_size();
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+    int retval;
+    gpt_protective_mbr_partition_record rec;
+
+    /* initialize the record. */
+    retval = gpt_protective_mbr_partition_record_init_span(&rec, nondet_size());
+    if (STATUS_SUCCESS != retval)
+    {
+        /* this method only fails with a bad size error. */
+        MODEL_ASSERT(FAT32_ERROR_GPT_BAD_SIZE == retval);
+        return 1;
+    }
+
+    return 0;
+}
