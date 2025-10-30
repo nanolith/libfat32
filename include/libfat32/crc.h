52c8144077e4f04405a1fcb1d5c7821806c57fc7
Justin Handville <nanolith@gmail.com>
2025-10-30 15:54:42 -0400

    crc32 decl.

---
+++ include/libfat32/crc.h
@@ -0,0 +1,34 @@
+/**
+ * \file libfat32/crc.h
+ *
+ * \brief CRC related functions.
+ *
+ * \copyright 2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <libfat32/function_decl.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief Calculates the CRC-32 of a given section of memory.
+ *
+ * \param data          Data array to CRC.
+ * \param size          Size of this array.
+ *
+ * \returns the CRC-32 of this section of memory.
+ */
+uint32_t FAT32_SYM(crc32)(const uint8_t* data, size_t size);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
