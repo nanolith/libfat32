7ed8601fc31be381e2d0da057db118b4e2f22bce
Justin Handville <nanolith@gmail.com>
2025-11-02 05:41:48 -0500

    Stub for test vector generator.

---
+++ build_src/crc32_testgen/main.c
@@ -0,0 +1,27 @@
+/**
+ * \file crc32_testgen/main.c
+ *
+ * \brief Generate test vectors for CRC-32 using a reference implementation in
+ * Z3.
+ *
+ * \copyright 2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <stdio.h>
+
+/**
+ * \brief Entry point for CRC-32 test vector generator.
+ *
+ * \param argc      Argument count (unused).
+ * \param argv      Argument vector (unused).
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    return 1;
+}
