cb5a529abfcd268e64ec3af7425c18b8e3ec941c
Justin Handville <nanolith@gmail.com>
2025-11-06 00:34:40 -0500

    Verify Z3 error code.

--- build_src/crc32_testgen/main.c
+++ build_src/crc32_testgen/main.c
@@ -876,7 +876,7 @@ static int crc_function_create(generator_context* ctx)
 
     /* create zero constant. */
     Z3_ast c0 = mk_bv_from_uint64(ctx, 32, 0);
-    if (NULL == c0)
+    if (NULL == c0 || Z3_OK != Z3_get_error_code(ctx->ctx))
     {
         fprintf(stderr, "error: could not create zero constant.\n");
         retval = 5;
